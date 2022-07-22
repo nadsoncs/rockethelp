9286210172cccad47c32daca596cea2477277528
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2022-07-22 09:07:01 -0300

    first commit

---
+++ src/screens/Details.tsx
@@ -0,0 +1,152 @@
+import { useEffect, useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+import { VStack, Text, useTheme, HStack, ScrollView, Box } from 'native-base';
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
+import { dateFormat } from '../utils/firestoreDateFormat';
+
+import { Header } from '../components/Header';
+import { OrderProps } from '../components/Order';
+import { Loading } from '../components/Loading';
+import { CircleWavyCheck, ClipboardText, DesktopTower, Hourglass } from 'phosphor-react-native';
+import { CardDetails } from '../components/CardDetails';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+import { Alert } from 'react-native';
+
+type RouteParams = {
+  orderId: string;
+}
+
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
+}
+export function Details() {
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
+  const [solution, setSolution] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  
+  const route = useRoute();
+  const navigation = useNavigation();
+  const { colors } = useTheme();
+  const { orderId } = route.params as RouteParams;
+
+  function handleOrderClose() {
+    if(!solution) {
+      return Alert.alert('Solicitação', 'Informe a solução para encerrar o solicitação.');
+    }
+
+    firestore()
+      .collection<OrderFirestoreDTO>('orders')
+      .doc(orderId)
+      .update({
+        status: 'closed',
+        solution,
+        closed_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        Alert.alert('Solicitação', 'Solicitação encerrada.');
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.error(error)
+        Alert.alert('Solicitação', 'Não foi possível encerrar a solicitação.')
+      })
+  }
+
+  useEffect(() => {
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .get()
+    .then((doc) => {
+      const { patrimony, description, status, created_at, closed_at, solution } = doc.data();
+      const closed = closed_at ? dateFormat(closed_at) : null;
+
+      setOrder({
+        id: doc.id,
+        patrimony,
+        description,
+        status,
+        solution,
+        when: dateFormat(created_at),
+        closed,
+      });
+      setIsLoading(false);
+    });
+
+  }, []);
+
+  if (isLoading) {
+    return <Loading/>
+  }
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Box px={6} bg="gray.600">
+        <Header title="Solicitação" />
+      </Box>
+
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {
+          order.status === 'closed' ?
+          <CircleWavyCheck size={22} color={colors.green[300]} /> :
+          <Hourglass size={22} color={colors.secondary[700]} />
+        }
+
+        <Text
+          fontSize="sm"
+          color={order.status === 'closed' ? colors.green[300]: colors.secondary[700]}
+          ml={2}
+          textTransform="uppercase"
+        >
+          {order.status === 'closed' ? 'finalizado' : 'em andamento'}
+        </Text>
+
+      </HStack>
+        
+      <ScrollView mx={5} showsVerticalScrollIndicator={false} >
+        <CardDetails
+          title="equipamento"
+          icon={DesktopTower}
+          description={`Património ${order.patrimony}`}
+        />
+
+        <CardDetails
+          title="descrição do problema"
+          icon={ClipboardText}
+          description={order.description}
+          footer={`Registrado em ${order.when}`}
+        />
+
+        <CardDetails
+          title="solução"
+          icon={CircleWavyCheck}
+          description={order.solution}
+          footer={order.closed && `Encerrado em ${order.closed}`}
+        >
+          {
+            order.status === 'open' &&
+            <Input
+              h={24}
+              textAlignVertical="top"
+              multiline
+              placeholder="Descrição da solução"
+              onChangeText={setSolution}
+            />
+          }
+        </CardDetails>
+      </ScrollView>
+
+      {
+        order.status === 'open' &&
+        <Button
+          title="Encerrar solicitação"
+          m={5}
+          onPress={handleOrderClose}
+        />
+      }
+    </VStack>
+  );
+}
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+import Logo from '../assets/logo_primary.svg';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+
+export function SignIn(){
+  const [isLoading, setIsLoading] = useState(false)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { colors } = useTheme();
+
+  function handleSignIn() {
+    if( !email || !password ) {
+      return Alert.alert('Login', 'Informe e-mail e senha.');
+    }
+    setIsLoading(true);
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('Login', 'E-mail inválido.');
+        }
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert('Login', 'E-mail ou senha inválida.');
+        }
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert('Login', 'Usuário não cadastrado.');
+        }
+
+        return Alert.alert('Login', 'Não foi possível acessar. Tente novamente mais tarde.')
+      })
+      .finally( ()=> setIsLoading(false));
+    
+  }
+  
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4}/>}
+        onChangeText={setEmail}
+      />
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4}/>}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
+    </VStack>
+  );
+}
